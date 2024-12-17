6f2bf87c3f1ed0977e951c1eed5a813c5f478b31
guilhermelp07 <guilpissaia@hotmail.com>
2024-12-17 20:03:52 -0300

    Validação no campo do tipo do movimento

--- api-estoques/src/main/java/com/utfpr/tikstok/api_estoques/dtos/EstoqueDTO.java
+++ api-estoques/src/main/java/com/utfpr/tikstok/api_estoques/dtos/EstoqueDTO.java
@@ -1,7 +1,9 @@
 package com.utfpr.tikstok.api_estoques.dtos;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 import java.util.Date;
 import java.util.List;
@@ -9,7 +11,8 @@ import java.util.List;
 public record EstoqueDTO(
         Long id,
         Date dtMovimento,
-        @NotNull(message = "Informe o tipo da movimentação (E - entrada)/S - saída)!")
+        @NotBlank(message = "Informe o tipo da movimentação (E - entrada)/S - saída)!")
+        @Pattern(regexp = "E|S", message = "O campo deve ser preenchido com 'E' - entrada ou 'S' - saída!")
         String tipo,
         Double valorTotal,
         @NotNull(message = "Informe os itens do lançamento!")
