b8349b62d6a269adc6c6e14364e99a1d07fc1928
guilhermelp07 <guilpissaia@hotmail.com>
2025-02-05 00:14:18 -0300

    remoção das classes item_estoque; estoque e produtos 1:1 (um produto para cada movimentação de estoque, descartando listas e tabela auxiliar dos itens)

--- api-estoques/src/main/java/com/utfpr/tikstok/api_estoques/services/EstoqueService.java
+++ api-estoques/src/main/java/com/utfpr/tikstok/api_estoques/services/EstoqueService.java
@@ -1,14 +1,11 @@
 package com.utfpr.tikstok.api_estoques.services;
 
 import com.utfpr.tikstok.api_estoques.dtos.EstoqueDTO;
-import com.utfpr.tikstok.api_estoques.dtos.ItemEstoqueDTO;
 import com.utfpr.tikstok.api_estoques.dtos.ProdutoDTO;
 import com.utfpr.tikstok.api_estoques.models.Estoque;
-import com.utfpr.tikstok.api_estoques.models.ItemEstoque;
 import com.utfpr.tikstok.api_estoques.repository.EstoqueRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -25,41 +22,26 @@ public class EstoqueService {
     }
 
     public Estoque lancarEstoque(EstoqueDTO estoqueDTO){
-        Double valorTotal = 0.0;
         Estoque estoque = new Estoque();
 
         estoque.setTipo(estoqueDTO.tipo());
         estoque.setDtMovimento(new Date());
 
-        List<ItemEstoque> itens = new ArrayList<>();
+        // Verifica se o produto existe no banco de dados
+        ProdutoDTO produtoBusca = produtoFeignClient.getProdutoById(estoqueDTO.idProduto());
 
-        for(ItemEstoqueDTO itemEstoqueDTO : estoqueDTO.itens()){
-
-            ProdutoDTO produtoBusca = produtoFeignClient.getProdutoById(itemEstoqueDTO.idProduto());
-
-            if(produtoBusca == null){
-                return null;
-            }
-            // Se for movimentacao de saida, deve verificar se o produto possui saldo para movimentar
-            if(estoqueDTO.tipo().equals("S")){
-                if(itemEstoqueDTO.quantidade() > produtoBusca.qtdEstoque())
-                    return null;
-            }
-
-            ItemEstoque itemEstoque = new ItemEstoque();
-
-            itemEstoque.setIdProduto(itemEstoqueDTO.idProduto());
-            itemEstoque.setQuantidade(itemEstoqueDTO.quantidade());
-            itemEstoque.setValor(itemEstoqueDTO.valor());
-            itemEstoque.setEstoque(estoque);
-
-            itens.add(itemEstoque);
-
-            valorTotal += itemEstoqueDTO.valor();
+        if(produtoBusca == null){
+            return null;
         }
-        estoque.setValorTotal(valorTotal);
+        // Se for movimentacao de saida, deve verificar se o produto possui saldo para movimentar
+        /*if(estoqueDTO.tipo().equals("S")){
+            if(estoqueDTO.quantidade() > produtoBusca.qtdEstoque())
+                return null;
+        }*/
 
-        estoque.setItens(itens);
+        estoque.setIdProduto(estoqueDTO.idProduto());
+        estoque.setQuantidade(estoqueDTO.quantidade());
+        estoque.setValorUnitario(estoqueDTO.valorUnitario());
 
         return this.estoqueRepository.save(estoque);
     }
