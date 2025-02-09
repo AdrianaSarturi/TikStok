533d828178c8b6c5fb6371dee474fe05586ecd86
AdrianaSarturi <adrianasarturi@alunos.utfpr.edu.br>
2025-02-09 14:12:23 -0300

    Adição de tratamento de exceções e ajsutes no swagger da API de produtos

--- api-produtos/src/main/java/com/utfpr/tikstok/api_produtos/exceptionhandler/CustomExceptionHandler.java
+++ api-produtos/src/main/java/com/utfpr/tikstok/api_produtos/exceptionhandler/CustomExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.utfpr.tikstok.api_produtos.exceptionhandler;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -7,18 +8,21 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<HashMap<String,String>> handleValidationErrors(MethodArgumentNotValidException ex){
-        HashMap<String,String> errors = new HashMap<>();
+    public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
 
-        for (FieldError fieldError : ex.getBindingResult().getFieldErrors()){
+        // Itera sobre os erros e captura o campo e mensagem
+        for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
             errors.put(fieldError.getField(), fieldError.getDefaultMessage());
         }
 
-        return ResponseEntity.badRequest().body(errors);
+        // Retorna o erro com o status 400
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 }
